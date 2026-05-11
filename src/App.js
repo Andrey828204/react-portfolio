b1cbdd4785672f4ae044a583e44757dfb8c7f69a
Andrey828204 <andre.tishch@gmail.com>
2026-05-11 10:35:08 +0300

    github pages routing

--- src/App.js
+++ src/App.js
@@ -9,19 +9,26 @@ import MyFuture from './pages/MyFuture';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/react-portfolio">
+
       <Header />
 
       <main className="container">
+
         <Routes>
           <Route path="/" element={<AboutMe />} />
+
           <Route path="/about" element={<AboutMe />} />
+
           <Route path="/my-city" element={<MyCity />} />
+
           <Route path="/my-future" element={<MyFuture />} />
         </Routes>
+
       </main>
 
       <Footer />
+
     </BrowserRouter>
   );
 }
